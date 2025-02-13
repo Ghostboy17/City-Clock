9d485f27fe2a17f936dbd8a7748becc1b31449c8
Ghostboy17 <fernandezcando17@gmail.com>
2025-02-13 15:56:13 -0500

    change

--- app.py
+++ app.py
@@ -99,7 +99,11 @@ def update_settings():
         conn.commit()
     return jsonify({"message": "Settings updated", "city": city, "color": color})
 
+
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
